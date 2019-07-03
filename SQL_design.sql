f3a7fdeb3b003c10c1563c5a48c4220470c098f9
RobertoHB <robherblancalpe@gmail.com>
2019-07-03 16:50:48 +0200

    Consultas con left/right join

--- SQL_design.sql
+++ SQL_design.sql
@@ -32,9 +32,25 @@ INSERT INTO viajaran (id_persona,id_lugar) VALUES
 INSERT INTO viajaran (id_lugar,id_persona) VALUES
   (2,4),(4,5),(5,3);
 
+
+INSERT INTO lugares (lugar) VALUES
+  ('Noja');
+
 ALTER TABLE viajaran ADD UNIQUE(id_persona,id_lugar);
 
+SELECT * FROM personas WHERE persona ='Isabel';
+
+SELECT * FROM viajaran WHERE id_person  a= 1;
+
+DELETE FROM viajaran WHERE id_viajaran = 1;
+
+INSERT INTO viajaran (id_lugar,id_persona) VALUES
+  (5,4);
 
 
+INSERT INTO viajaran (id_lugar,id_persona) VALUES
+  (3,4);
+
+SELECT * FROM lugares;
 
 
---
+++ consultas.sql
@@ -0,0 +1,52 @@
+﻿-- (01) Lugares que no visitará nadie
+
+
+ SELECT lugar FROM lugares left JOIN viajaran USING(id_lugar) WHERE viajaran.id_lugar IS NULL;
+
+
+-- (02) A que destinos va cada persona.
+
+  
+  SELECT persona, lugar FROM viajaran JOIN lugares USING(id_lugar) JOIN personas USING(id_persona);
+
+
+  -- (03) ¿ que personas no viajarán?
+
+   SELECT persona FROM viajaran  RIGHT JOIN personas ON personas.id_persona= viajaran.id_persona WHERE viajaran.id_persona IS NULL;
+
+
+    -- (04) Persona que más viajará
+
+
+
+
+
+    -- (05) lugar más visitado
+
+
+-- c1
+SELECT id_lugar,COUNT(*)n FROM viajaran GROUP BY id_lugar;
+
+
+-- (06) Destino al que viajarán  todas las personas que viajan
+
+
+
+
+
+
+-- (07) Personas que viajarán a todos los destinos
+
+
+SELECT persona FROM (
+    SELECT id_persona FROM viajaran GROUP BY 1
+     HAVING COUNT(*) = (
+        SELECT COUNT(DISTINCT id_lugar) FROM viajaran
+    )
+    )c3 JOIN personas USING(id_persona);
+
+
+
+
+
+
